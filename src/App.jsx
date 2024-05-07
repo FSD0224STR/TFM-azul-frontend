48f0a61674296061bf0695d10264acae1a8976a4
Alvaroogallar <alvaro.ogallarb@gmail.com>
2024-05-07 17:24:02 +0200

    recordarLogeo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route, useNavigate} from 'react-router-dom';
 import './App.css'; // Importa el archivo de estilos CSS
 
 import LoginForm from './pages/login';
 import RegistrationForm from './pages/registration';
 import MainPage from './pages/mainpage';
+import { useEffect } from 'react';
 
 
 // Componente de la aplicación principal
 export default function App() {
+
+  const navigate = useNavigate ()
+
+  useEffect (() => {
+    const token = localStorage.getItem('access_token')
+    if (!token) navigate ('/')
+      else navigate ('/login')
+  }, [])
+
   return (
     <>
       <div className="container">
