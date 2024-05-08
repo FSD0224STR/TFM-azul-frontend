8bca7c6b460c4d2bd60494c95051b071f32b7b66
Bet Oliver <eoliveco11@gmail.com>
2024-05-08 16:43:39 +0200

    actualizado con AuthContext

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,48 +1,70 @@
-import { Form, Input, Button } from 'antd';
-import NavFooter from '../components/navFooter';
-import { useState } from 'react';
-import Password from 'antd/es/input/Password';
-import { login } from '../apiservice/userApi';
-import { useNavigate } from 'react-router-dom/dist';
+import { useState, useContext } from "react";
+
+import { Form, Input, Button, Spin, Alert } from "antd";
+import NavFooter from "../components/navFooter";
+import Password from "antd/es/input/Password";
+import { AuthContext } from "../contexts/authContext";
 
 // Componente del formulario de inicio de sesión
 const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState ('')
-    const [password, setPassword] = useState ('')
+  const { login, loading, error, setError } = useContext(AuthContext);
 
-    const navigate = useNavigate()
+  return (
+    <>
+      <Form name="login" layout="vertical">
+        <h2>Iniciar sesión</h2>
+        <Form.Item
+          label="Correo electrónico"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Por favor ingresa tu correo electrónico",
+            },
+          ]}
+        >
+          <Input
+            value={username}
+            onChange={(e) => setUsername(e.currentTarget.value)}
+          />
+        </Form.Item>
 
-    const handleLogin = async () => {
-      // Aquí puedes enviar una solicitud al servidor para autenticar al usuario
-      const response = await login(username, password)
-      console.log('A ver el token:', response.token);
-      console.log ('A ver el id del usuario', response.id)
-      localStorage.setItem('access_token', response.token)
-      localStorage.setItem ('id', response.id)
-      navigate('/home')
-    };
-  
-    return (
-      <>
-        <Form  name="login" onSubmit={handleLogin} layout="vertical">
-          <h2>Iniciar sesión</h2>
-          <Form.Item label="Correo electrónico" name="email" rules={[{ required: true, message: 'Por favor ingresa tu correo electrónico' }]}>
-            <Input value={username} onChange={e => setUsername (e.currentTarget.value)} />
-          </Form.Item>
+        <Form.Item
+          label="Contraseña"
+          name="password"
+          rules={[
+            { required: true, message: "Por favor ingresa tu contraseña" },
+          ]}
+        >
+          <Input.Password
+            value={Password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          />
+        </Form.Item>
 
-          <Form.Item label="Contraseña" name="password" rules={[{ required: true, message: 'Por favor ingresa tu contraseña' }]}>
-            <Input.Password value={Password} onChange={e => setPassword (e.currentTarget.value)} />
-          </Form.Item>
-
-          <Form.Item>
-            <Button type="primary" htmlType="submit" onClick={handleLogin}  block>
-              Iniciar sesión
+        <Form.Item>
+          {loading ? (
+            <Spin />
+          ) : (
+            <Button type="primary" onClick={() => login(username, password)}>
+              Login
             </Button>
-          </Form.Item>
-        </Form>
-        <NavFooter />
-      </>
-    );
-  };
-  export default LoginForm;
+          )}
+        </Form.Item>
+      </Form>
+      {error && (
+        <Alert
+          message={`Ha habido un error: ${error}`}
+          type="error"
+          closable
+          onClose={() => setError("")}
+        />
+      )}
+      <NavFooter />
+    </>
+  );
+};
+export default LoginForm;
