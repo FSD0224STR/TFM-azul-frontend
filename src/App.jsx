284c85599e9c535b0ffb53e1171822580079468b
Bet Oliver <eoliveco11@gmail.com>
2024-05-08 16:42:06 +0200

    Actualizado con AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import { Routes, Route, useNavigate} from 'react-router-dom';
-import './App.css'; // Importa el archivo de estilos CSS
+import { Routes, Route } from "react-router-dom";
+import "./App.css"; // Importa el archivo de estilos CSS
 
-import LoginForm from './pages/login';
-import RegistrationForm from './pages/registration';
-import MainPage from './pages/mainpage';
-import { useEffect } from 'react';
+import LoginForm from "./pages/login";
+import RegistrationForm from "./pages/registration";
+import MainPage from "./pages/mainpage";
 
+import { AuthContext } from "./contexts/authContext";
+import { useContext } from "react";
 
-// Componente de la aplicación principal
 export default function App() {
-
-  const navigate = useNavigate ()
-
-  useEffect (() => {
-    const token = localStorage.getItem('access_token')
-    if (!token) navigate ('/')
-      else navigate ('/login')
-  }, [])
+  const { loading } = useContext(AuthContext);
+  if (loading) return <h1>Loading...</h1>;
 
   return (
     <>
@@ -25,8 +19,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<LoginForm />} />
             <Route path="/registro" element={<RegistrationForm />} />
-            <Route path="/home" element={<MainPage/>} />
-            
+            <Route path="/home" element={<MainPage />} />
           </Routes>
         </div>
       </div>
