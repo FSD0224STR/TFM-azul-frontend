9012dc305b0ee9444ada96f3178653f41af37feb
Bet Oliver <eoliveco11@gmail.com>
2024-05-14 12:38:20 +0200

    mejora visibilidad TripCard y ahora muestra descripción y owner

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,15 +1,22 @@
 import { Button, Card, Typography } from "antd";
-import '../styles/TripCard.css';
+import "../styles/TripCard.css";
 
-export const TripCard = ({ title, start_date, end_date, user, onDelete }) => {
+export const TripCard = ({
+  title,
+  start_date,
+  end_date,
+  description,
+  owner,
+  onDelete,
+}) => {
   return (
     <Card className="cardSize">
       <div className="cardBody">
-        <Typography.Title
-                level={3}>{title}</Typography.Title>
+        <Typography.Title level={3}>{title}</Typography.Title>
         <Typography variant="body2">{start_date}</Typography>
         <Typography variant="body2">{end_date}</Typography>
-        <Typography variant="body2">{user}</Typography>
+        <Typography variant="body2">{description}</Typography>
+        <Typography variant="body2">{owner}</Typography>
         <Button onClick={onDelete}>Borrar</Button>
       </div>
     </Card>
