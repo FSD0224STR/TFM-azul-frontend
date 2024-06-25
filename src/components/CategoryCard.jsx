7b6b78adaa626053299838c6053e457da64c2385
Bet Oliver <eoliveco11@gmail.com>
2024-06-25 15:59:42 +0200

    onEdit cambia icono para confirmar edición

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 import { Card, Typography, Input, Modal, message, Popconfirm } from "antd";
-import {
-  DeleteOutlined,
-  EditOutlined,
-  // FileSearchOutlined,
-} from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, CheckOutlined } from "@ant-design/icons";
 import "../styles/CategoryCard.css";
 import categoryApi from "../apiservice/categoryApi";
 import { useNavigate } from "react-router-dom";
@@ -14,6 +10,7 @@ export const CategoryCard = ({ id, title, refreshCategories }) => {
   const [error, setError] = useState("");
   const [newTitle, setNewTitle] = useState(title);
   const [loading, setLoading] = useState(false);
+  const [editIcon, setEditIcon] = useState(<EditOutlined />);
   const navigate = useNavigate();
 
   const showErrorModal = (message) => {
@@ -37,67 +34,76 @@ export const CategoryCard = ({ id, title, refreshCategories }) => {
           showErrorModal(response.error);
         } else {
           console.log(
-            "Esta es la respuesta de actualizar la categoría:",
+            "Respuesta de actualizar la categoría:",
             response.message
           );
-          setUpdatingCategory(!updatingCategory); // Mover esta línea aquí
+          setUpdatingCategory(false);
           refreshCategories();
-          setLoading(false);
         }
       } catch (error) {
         setError(error.message);
         console.log(error.message);
         showErrorModal(error.message);
+      } finally {
+        setLoading(false);
       }
     } else {
-      setUpdatingCategory(!updatingCategory);
+      setUpdatingCategory(true);
     }
   };
 
-  const onDelete = async (e) => {
-    e.stopPropagation();
+  const onDelete = async () => {
     setLoading(true);
     try {
       const response = await categoryApi.deleteCategory(id);
-      console.log("La respuesta de borrar es:", response);
+      console.log("Respuesta de borrar la categoría:", response);
       if (response.error) {
         setError(response.error);
         console.log(response.error);
         showErrorModal(response.error);
       } else {
-        console.log(
-          "Esta es la respuesta de eliminar la categoría:",
-          response.message
-        );
+        message.success("Categoría eliminada");
         refreshCategories();
       }
     } catch (error) {
       setError(error.message);
       showErrorModal(error.message);
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
   };
 
-  const confirm = (e) => {
-    console.log(e);
-    message.success("Un viaje menos, una depresión más");
+  const confirmDelete = (e) => {
+    e.stopPropagation(); // Detener la propagación del evento hacia arriba
     onDelete();
   };
-  const cancel = (e) => {
-    console.log(e);
-    message.error("Una retirada a tiempo es una victoria");
+
+  const cancelDelete = (e) => {
+    e.stopPropagation(); // Detener la propagación del evento hacia arriba
+    message.info("Operación cancelada");
   };
-  const onView = () => {
+
+  const onView = (e) => {
+    e.stopPropagation(); // Detener la propagación del evento hacia arriba
     navigate(`/categories/${id}`);
+  };
+
+  const toggleEdit = (e) => {
+    e.stopPropagation();
+    if (updatingCategory) {
+      onEdit(e);
+    } else {
+      setUpdatingCategory(true);
+      setEditIcon(<CheckOutlined />);
+    }
   };
 
   return (
     <Card
       className="cardSize categoryCard"
       style={{ margin: "25px", width: "20%" }}
-      onClick={onView}
     >
-      <div className="categoryPanel">
+      <div className="categoryPanel" onClick={onView}>
         <div className="categoryTitle">
           {updatingCategory ? (
             <Input
@@ -105,6 +111,7 @@ export const CategoryCard = ({ id, title, refreshCategories }) => {
               placeholder={title}
               onChange={(e) => setNewTitle(e.target.value)}
               className="titleInput"
+              onClick={(e) => e.stopPropagation()} // Evitar que el clic se propague hacia arriba
             />
           ) : (
             <Typography.Title level={4} className="cardTitle">
@@ -114,27 +121,28 @@ export const CategoryCard = ({ id, title, refreshCategories }) => {
         </div>
         <div className="btns">
           <Popconfirm
-            title="Borrar el viaje"
-            description="¿Estás seguro que quieres borrar este viaje?"
-            onConfirm={confirm}
-            onCancel={cancel}
+            title="¿Estás seguro que quieres borrar este viaje?"
+            onConfirm={confirmDelete}
+            onCancel={cancelDelete}
             okText="Sí"
             cancelText="No"
           >
             <DeleteOutlined
-              className="icon-size danger-color "
-              onClick={onDelete}
+              className="icon-size danger-color"
+              onClick={(e) => e.stopPropagation()}
             />
           </Popconfirm>
 
-          <EditOutlined
-            onClick={onEdit}
-            className="icon-size icon-margin-left"
-          />
-          {/* <FileSearchOutlined
-            onClick={onView}
-            className="icon-size icon-margin-left"
-          /> */}
+          {updatingCategory ? (
+            <span className="icon-size icon-margin-left" onClick={toggleEdit}>
+              {editIcon}
+            </span>
+          ) : (
+            <EditOutlined
+              className="icon-size icon-margin-left"
+              onClick={toggleEdit}
+            />
+          )}
         </div>
       </div>
     </Card>
