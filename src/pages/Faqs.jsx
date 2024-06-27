a39e39b3fe59f5ed255a7cab92f5ebd7f6d51ca7
Bet Oliver <eoliveco11@gmail.com>
2024-06-27 16:37:15 +0200

    mejoro texto FAQ

--- src/pages/Faqs.jsx
+++ src/pages/Faqs.jsx
@@ -1,73 +1,83 @@
 import { CaretRightOutlined, ShareAltOutlined } from "@ant-design/icons";
 import { Collapse, theme } from "antd";
-
 const getItems = (panelStyle) => [
   {
     key: "1",
-    label: "Cómo puedo invitar a mis compañeros al viaje?",
+    label: "¿Cómo puedo invitar a otros usuarios a unirse a mi viaje?",
     children: (
       <p>
-        Es muy fácil, sólo tienes que copiar el enlace del viaje que encontrarás
-        en el icono compartir <ShareAltOutlined /> y mandarlo a las personas que
-        quieres que se vinculen al viaje que has creado.
+        Para invitar a otros usuarios a tu viaje, copia el enlace de invitación
+        que encontrarás al hacer clic en el ícono de compartir{" "}
+        <ShareAltOutlined /> en la parte superior de la página del viaje. Luego,
+        envía este enlace a las personas con las que deseas compartir tu viaje
+        para que puedan unirse fácilmente.
+        <br />
+        <br />
+        Es importante que los invitados tengan una cuenta registrada y estén
+        logueados para poder unirse al viaje.
       </p>
     ),
     style: panelStyle,
   },
   {
     key: "2",
-    label: "Mis compañeros podrán invitar a un viaje que he creado yo?",
+    label:
+      "¿Mis compañeros pueden invitar a otros usuarios a un viaje que he creado?",
     children: (
       <p>
-        Sí, todas los usuarios que esten vinculados a un viaje tienen acceso al
-        link de vinculación. El único requisito para vincularse a un viaje es
-        tener una cuenta. Antes de vincularte al viaje deberás registrarte o
-        iniciar sesión si ya tienes cuenta.
+        Sí, todos los usuarios vinculados a un viaje tienen acceso al enlace de
+        invitación. Para unirse a un viaje, es necesario tener una cuenta
+        registrada. Si aún no tienen una cuenta, deberán registrarse antes de
+        poder vincularse al viaje.
       </p>
     ),
     style: panelStyle,
   },
   {
     key: "3",
-    label: "Quién puede añadir categorías y propuestas al viaje?",
+    label: "¿Quién puede añadir categorías y propuestas al viaje?",
     children: (
-      <p>Todos los usuarios que esten vinculados al viaje en cuestión. </p>
+      <p>
+        Todos los usuarios vinculados al viaje pueden añadir categorías y
+        propuestas.
+      </p>
     ),
     style: panelStyle,
   },
   {
     key: "4",
-    label: "Quién puede borrar categorías y propuestas al viaje?",
+    label: "¿Quién puede borrar categorías y propuestas del viaje?",
     children: (
       <p>
-        Las categorías que tengan propuestas asociadas no se podran borrar. La
-        aplicación sólo permite borrar aquellas categorías que no contienen
-        ninguna propuesta.
+        Solo se pueden borrar las categorías que no tienen propuestas asociadas.
+        Si una categoría tiene propuestas, no se podrá eliminar hasta que todas
+        las propuestas sean eliminadas. Las propuestas sólo pueden ser borradas
+        por el usuario que las creó.
       </p>
     ),
     style: panelStyle,
   },
   {
     key: "5",
-    label: "Ya no quiero ver más un viaje, lo puedo borrar?",
+    label: "¿Puedo eliminar un viaje si ya no deseo verlo?",
     children: (
       <p>
-        Si hay otros usuarios vinculados al viaje, no lo podrás borrar. Puede
-        que a ellos sí les interese guardar la información del viaje. Si no
-        quieres ver más el viaje, tienes la opción de desvincularte. Si te
-        desvinculas ya no te parecerá más ese viaje en tu lista de viajes. Si no
-        hay otros usuarios vinculados, puedes borrarlo.
+        Si otros usuarios están vinculados al viaje, no podrás eliminarlo
+        completamente. Sin embargo, puedes desvincularte del viaje para dejar de
+        verlo en tu lista personal. Si no hay otros usuarios vinculados, tienes
+        la opción de borrar el viaje por completo.
       </p>
     ),
     style: panelStyle,
   },
   {
     key: "6",
-    label: "Puedo volver a vincularme a un viaje que ya he desvinculado?",
+    label: "¿Puedo volver a vincularme a un viaje del que me he desvinculado?",
     children: (
       <p>
-        Sí, pero tendrás que pedir a algun usuario que siga vinculado, que te
-        mande el link para poder vincularte.
+        Sí, puedes volver a vincularte a un viaje del que te has desvinculado.
+        Para hacerlo, solicita a algún usuario vinculado que te comparta el
+        enlace de invitación al viaje.
       </p>
     ),
     style: panelStyle,
@@ -77,7 +87,7 @@ const FAQs = () => {
   const { token } = theme.useToken();
   const panelStyle = {
     marginBottom: 24,
-    background: token.colorFillAlter,
+    background: "rgba(0, 81, 255, 0.100)",
     borderRadius: token.borderRadiusLG,
     border: "none",
   };
@@ -85,7 +95,6 @@ const FAQs = () => {
     <Collapse
       className="cardInfoTrip"
       bordered={false}
-      defaultActiveKey={["1"]}
       expandIcon={({ isActive }) => (
         <CaretRightOutlined rotate={isActive ? 90 : 0} />
       )}
