a9ea49330defc40180452818270fe5413eb2a63d
Bet Oliver <eoliveco11@gmail.com>
2024-06-28 11:09:58 +0200

    estilos registro

--- src/pages/registration2.jsx
+++ src/pages/registration2.jsx
@@ -2,6 +2,7 @@ import { Typography, Form, Input, Button, Alert, Modal } from "antd";
 import userApi from "/src/apiservice/userApi.js";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import "../styles/Registration.css";
 
 const RegistrationForm2 = () => {
   const [form] = Form.useForm();
@@ -36,123 +37,130 @@ const RegistrationForm2 = () => {
     length && uppercase && lowercase && number && specialChar && !error;
 
   return (
-    <Form
-      className="cardInfoTrip"
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      layout="vertical"
-      style={{
-        maxWidth: 600,
-        margin: "auto",
-      }}
-      scrollToFirstError
-    >
-      <Typography.Title
-        level={2}
-        style={{ textAlign: "center", marginBottom: 24 }}
+    <div className="cardInfoTrip">
+      <Form
+        className="registration-form"
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        layout="horitzontal"
+        style={{
+          maxWidth: 600,
+          margin: "auto",
+        }}
+        scrollToFirstError
       >
-        Registro
-      </Typography.Title>
-
-      <Form.Item
-        label="Nombre de usuario"
-        name="username"
-        rules={[
-          { required: true, message: "Por favor ingresa tu Nombre de usuario" },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Nombre"
-        name="firstname"
-        rules={[{ required: true, message: "Por favor ingresa tu nombre" }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Apellidos"
-        name="lastname"
-        rules={[{ required: true, message: "Por favor ingresa tus apellidos" }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Correo electrónico"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Por favor ingresa tu correo electrónico",
-          },
-          { type: "email", message: "El correo electrónico no es válido" },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Contraseña"
-        name="password"
-        rules={[
-          { required: true, message: "Por favor ingresa tu contraseña" },
-          {
-            validator: (_, value) => {
-              const requirements = {
-                length: value.length >= 8,
-                uppercase: /[A-Z]/.test(value),
-                lowercase: /[a-z]/.test(value),
-                number: /\d/.test(value),
-                specialChar: /[@$!%*?&]/.test(value),
-              };
-              return isFormValid(requirements)
-                ? Promise.resolve()
-                : Promise.reject(
-                    new Error(
-                      "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial (@$!%*?&)"
-                    )
-                  );
-            },
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" block>
-          Registrarse
-        </Button>
-      </Form.Item>
-
-      {error && (
-        <Alert
-          type="error"
-          message={error.error}
-          style={{ marginBottom: 24 }}
-          showIcon
-        />
-      )}
-
-      {addedUser && (
-        <Modal
-          title="Registro exitoso"
-          open={modalVisible}
-          onOk={handleModalOk}
-          onCancel={() => setModalVisible(false)}
+        <Typography.Title
+          level={2}
+          style={{ textAlign: "center", marginBottom: 24 }}
         >
-          <p>
-            Te has registrado correctamente. Inicia sesión para empezar a crear
-            viajes.
-          </p>
-        </Modal>
-      )}
-    </Form>
+          Registro
+        </Typography.Title>
+
+        <Form.Item
+          label="Nombre de usuario"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Por favor ingresa tu Nombre de usuario",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Nombre"
+          name="firstname"
+          rules={[{ required: true, message: "Por favor ingresa tu nombre" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Apellidos"
+          name="lastname"
+          rules={[
+            { required: true, message: "Por favor ingresa tus apellidos" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Correo electrónico"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Por favor ingresa tu correo electrónico",
+            },
+            { type: "email", message: "El correo electrónico no es válido" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Contraseña"
+          name="password"
+          rules={[
+            { required: true, message: "Por favor ingresa tu contraseña" },
+            {
+              validator: (_, value) => {
+                const requirements = {
+                  length: value.length >= 8,
+                  uppercase: /[A-Z]/.test(value),
+                  lowercase: /[a-z]/.test(value),
+                  number: /\d/.test(value),
+                  specialChar: /[@$!%*?&]/.test(value),
+                };
+                return isFormValid(requirements)
+                  ? Promise.resolve()
+                  : Promise.reject(
+                      new Error(
+                        "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial (@$!%*?&)"
+                      )
+                    );
+              },
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item className="registration-btn">
+          <Button type="primary" htmlType="submit" block>
+            Registrarse
+          </Button>
+        </Form.Item>
+
+        {error && (
+          <Alert
+            type="error"
+            message={error.error}
+            style={{ marginBottom: 24 }}
+            showIcon
+          />
+        )}
+
+        {addedUser && (
+          <Modal
+            title="Registro exitoso"
+            open={modalVisible}
+            onOk={handleModalOk}
+            onCancel={() => setModalVisible(false)}
+          >
+            <p>
+              Te has registrado correctamente. Inicia sesión para empezar a
+              crear viajes.
+            </p>
+          </Modal>
+        )}
+      </Form>
+    </div>
   );
 };
 
