915cb822a5bb91740debde373c361c3a4771072e
Bet Oliver <eoliveco11@gmail.com>
2024-06-28 14:35:24 +0200

    creo página base

---
+++ src/pages/confirmRegistration.jsx
@@ -0,0 +1,18 @@
+import { Typography, Button } from "antd";
+
+const { Title, Text } = Typography;
+
+const ValidationTripPage = () => {
+  return (
+    <div className="cardInfoTrip">
+      <Title>¿Ya te has registrado?</Title>
+      <Text>
+        Haz click al botón para confirmar tu registro y así poder iniciar
+        sesión.
+      </Text>
+      <Button type="primary">Valida tu cuenta</Button>
+    </div>
+  );
+};
+
+export default ValidationTripPage;
