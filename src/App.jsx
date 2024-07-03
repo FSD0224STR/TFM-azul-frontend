c31b72514834b9ba280c4e653a112ff3fa765d7d
Sebastian <158612272+BAchxz@users.noreply.github.com>
2024-07-03 02:03:56 +0200

    NodeMailer

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,47 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/login";
-//import Registration from "./pages/registration";
 import UserDetails from "./pages/UserDetails";
-
 import { AuthContext } from "./contexts/authContext";
 import { useContext } from "react";
 import { NavBar2 } from "./components/NavBar2";
-
 import "./styles/App.css";
 import { Trip } from "./pages/Trip";
 import { ViewCategory } from "./pages/viewCategory";
 import JoinTripPage from "./pages/JoinTrip";
 import FAQs from "./pages/Faqs";
 import RegistrationForm2 from "./pages/registration2";
+import ConfirmRegistration from "./pages/confirmRegistration"; // Importa la nueva página
 import { Spin } from "antd";
 
 export const App = () => {
   const { loading } = useContext(AuthContext);
-  if (loading)
+
+  if (loading) {
     return (
       <>
-        <NavBar2></NavBar2>
+        <NavBar2 />
         <div className="cardInfoTrip">
-          <Spin size="large" />;
+          <Spin size="large" />
         </div>
       </>
     );
+  }
+
   return (
     <>
-      <NavBar2></NavBar2>
+      <NavBar2 />
       <Routes>
-        <Route path="/home" element={<Home props></Home>}></Route>
-
-        <Route
-          path="/perfil"
-          element={<UserDetails props></UserDetails>}
-        ></Route>
-
-        <Route path="/" element={<Login props></Login>}></Route>
-
-        <Route path="/home" element={<Home props></Home>}></Route>
-
-        <Route path="/:id" element={<Trip props></Trip>}></Route>
-
-        <Route
-          path="categories/:id"
-          element={<ViewCategory props></ViewCategory>}
-        ></Route>
-
-        <Route
-          path="/registration"
-          element={<RegistrationForm2 props></RegistrationForm2>}
-        ></Route>
-
-        <Route path="/join-trip/:id" element={<JoinTripPage />}></Route>
-
-        <Route path="/faqs" element={<FAQs />}></Route>
+        <Route path="/home" element={<Home />} />
+        <Route path="/perfil" element={<UserDetails />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/:id" element={<Trip />} />
+        <Route path="categories/:id" element={<ViewCategory />} />
+        <Route path="/registration" element={<RegistrationForm2 />} />
+        <Route path="/confirm-registration" element={<ConfirmRegistration />} /> {/* Nueva ruta */}
+        
+        <Route path="/join-trip/:id" element={<JoinTripPage />} />
+        <Route path="/faqs" element={<FAQs />} />
       </Routes>
     </>
   );
