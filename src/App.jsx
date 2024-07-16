2955bb5fb111b63342749ca66ffc485194b3a9ee
Bet Oliver <eoliveco11@gmail.com>
2024-07-16 11:07:22 +0200

    borro import q no existía

--- src/App.jsx
+++ src/App.jsx
@@ -11,69 +11,57 @@ import { ViewCategory } from "./pages/viewCategory";
 import JoinTripPage from "./pages/JoinTrip";
 import FAQs from "./pages/Faqs";
 import RegistrationForm2 from "./pages/registration2";
-import ConfirmValidation from "./pages/ConfirmValidation"; 
-import ConfirmRegistration from "./pages/confirmRegistration"; // Importa la nueva página
+import ConfirmValidation from "./pages/ConfirmValidation";
 import { Spin, message } from "antd";
 import ForgotPassword from "./pages/forgotPassword";
 import ResetPassword from "./pages/resetPassword";
 //websockets
-import { socket } from './socket';
-
-
-
+import { socket } from "./socket";
 
 export const App = () => {
   const { loading } = useContext(AuthContext);
   //websockets
-  
-  const [userToDisconnect, setUserToDisconnect] = useState(localStorage.getItem('userId') || "");
+
+  const [userToDisconnect, setUserToDisconnect] = useState(
+    localStorage.getItem("userId") || ""
+  );
 
   const [messageApi, contextHolder] = message.useMessage();
 
   useEffect(() => {
-    const userId = localStorage.getItem('userId');
+    const userId = localStorage.getItem("userId");
     function onConnect() {
       if (userId !== null) {
         setUserToDisconnect(userId);
-        console.log ("este el es id del usuario", userId)
-        socket.emit('connection', userId); // Enviar userId al servidor al conectar
+        console.log("este el es id del usuario", userId);
+        socket.emit("connection", userId); // Enviar userId al servidor al conectar
       }
     }
 
     function onDisconnect() {
-      
       console.log("desconectando usuario", userToDisconnect);
-      socket.emit('disconnect', userToDisconnect); // Enviar userId al servidor al desconectar
+      socket.emit("disconnect", userToDisconnect); // Enviar userId al servidor al desconectar
     }
 
-  
-
-    function onUpdate (data) {
+    function onUpdate(data) {
       console.log("actualizando un viaje", data);
       messageApi.open({
-        type: 'success',
+        type: "success",
         content: `${data}`,
         duration: 10,
       });
-      
     }
 
-    
-
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('message', onUpdate); // Escuchar el evento 'tripUpdated'
-    
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("message", onUpdate); // Escuchar el evento 'tripUpdated'
 
     return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off ('message', onUpdate)
-      
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("message", onUpdate);
     };
   }, []);
-
-
 
   if (loading) {
     return (
@@ -90,7 +78,7 @@ export const App = () => {
     <>
       <NavBar2 />
       {contextHolder}
-      
+
       <Routes>
         <Route exact path="/about" element={<Login />} />
         <Route path="/home" element={<Home />} />
@@ -99,7 +87,10 @@ export const App = () => {
         <Route path="/trip/:id" element={<Trip />} />
         <Route path="categories/:id" element={<ViewCategory />} />
         <Route path="/registration" element={<RegistrationForm2 />} />
-        <Route path="/confirm-validation/:token" element={<ConfirmValidation />} />
+        <Route
+          path="/confirm-validation/:token"
+          element={<ConfirmValidation />}
+        />
         <Route path="/join-trip/:id" element={<JoinTripPage />} />
         <Route path="/faqs" element={<FAQs />} />
         <Route path="/forgot-password/" element={<ForgotPassword />} />
