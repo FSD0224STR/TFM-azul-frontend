435f3584343f7fea443d3897af3f7d314fa003dd
Bet Oliver <eoliveco11@gmail.com>
2024-07-17 13:46:02 +0200

    estilos Trip

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -103,7 +103,7 @@ export const CategoryCard = ({ id, title, refreshCategories }) => {
       style={{ margin: "15px 0px", padding: "0", width: "100%" }}
     >
       <div className="content-card" onClick={onView}>
-        <div className="category-title">
+        <div>
           {updatingCategory ? (
             <Input
               value={newTitle}
@@ -111,7 +111,6 @@ export const CategoryCard = ({ id, title, refreshCategories }) => {
               onChange={(e) => setNewTitle(e.target.value)}
               className="titleInput"
               onClick={(e) => e.stopPropagation()}
-              style={{ fontSize: "20px", color: "#333", textAlign: "" }}
             />
           ) : (
             <Typography.Title level={4} className="cardTitle">
